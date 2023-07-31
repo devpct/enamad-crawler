ae62c8e59ecf808c30498a68f9eb35ae6ecef08b
Mohammad Abdollahzadeh <mr.mohammad.pg19@gmail.com>
2023-07-31 16:29:56 +0330

    debug

--- src/controllers/csvController.ts
+++ src/controllers/csvController.ts
@@ -1,45 +1,48 @@
-import { Request, Response } from 'express'
-import { unparse } from 'papaparse'
-import { IBusiness } from '../interfaces/IBusiness'
-import Business from '../models/businessModel'
+import { Request, Response } from 'express';
+import { unparse } from 'papaparse';
+import { IBusiness } from '../interfaces/IBusiness';
+import Business from '../models/businessModel';
 
 const downloadCSV = (req: Request, res: Response) => {
-  const fields = req.query.fields as string
+  const fields = req.query.fields as string;
   if (!fields) {
     // If the 'fields' parameter is not provided, return all data from the table
     Business.find({})
-      .lean()
-      .then((businesses: Array<IBusiness & { _id: any }>) => {
-        const csvString = unparse(businesses, {
-          header: true,
-        })
-        res.setHeader('Content-Type', 'text/csv')
-        res.setHeader('Content-Disposition', 'attachment filename=exported_data.csv')
-        res.send(csvString)
-      })
-      .catch((error) => {
-        console.error(error)
-        res.status(500).json({ message: 'Internal Server Error' })
-      })
+    .select(fields)
+    .lean()
+    .then((businesses: Array<IBusiness>) => {
+      const csvString = unparse(businesses, {
+        header: true,
+      });
+      res.setHeader('Content-Type', 'text/csv');
+      res.setHeader('Content-Disposition', 'attachment; filename="exported_data.csv"');
+      res.send(csvString);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    });
+  
   } else {
     // If the 'fields' parameter is provided, return only the selected fields
-    const selectedFields = fields.split(',')
+    const selectedFields = fields.split(',');
     Business.find({})
       .select(selectedFields.join(' '))
       .lean()
-      .then((businesses: Array<IBusiness & { _id: any }>) => {
+      .then((businesses: Array<IBusiness>) => {
         const csvString = unparse(businesses, {
           header: true,
-        })
-        res.setHeader('Content-Type', 'text/csv')
-        res.setHeader('Content-Disposition', 'attachment filename=exported_data.csv')
-        res.send(csvString)
+        });
+        res.setHeader('Content-Type', 'text/csv');
+        res.setHeader('Content-Disposition', 'attachment; filename="exported_data.csv"'); 
+
+        res.send(csvString);
       })
       .catch((error) => {
-        console.error(error)
-        res.status(500).json({ message: 'Internal Server Error' })
-      })
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+      });
   }
-}
+};
 
-export { downloadCSV }
+export { downloadCSV };
--- src/interfaces/IBusiness.ts
+++ src/interfaces/IBusiness.ts
@@ -1,8 +1,24 @@
+import { Document, Schema, model, Types } from 'mongoose';
+
 export interface IBusiness extends Document {
-    domainAddress: string
-    businessTitle: string
-    city: string
-    stars: number
-    grantDate: string
-    expiryDate: string
+  domainAddress: string;
+  businessTitle: string;
+  city: string;
+  stars: number;
+  grantDate: string;
+  expiryDate: string;
+  _id: Types.ObjectId;
 }
+
+const businessSchema = new Schema<IBusiness>({
+  domainAddress: { type: String, required: true },
+  businessTitle: { type: String, required: true },
+  city: { type: String, required: true },
+  stars: { type: Number, required: true },
+  grantDate: { type: String, required: true },
+  expiryDate: { type: String, required: true },
+});
+
+const Business = model<IBusiness>('Business', businessSchema);
+
+export default Business;
